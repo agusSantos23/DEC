e1a4d64668319803eb87cdb43b758df19a46c3f9
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-12-02 13:16:35 +0100

    modificaciones de estilo, manejo de errores

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ const getDB = (type, filters = {}) => {
     }
 
     // Agregamos la reserva a la base de datos
-    db.bookings.push(new Reserva(cliente, viaje, dato.fecha))
+    db.bookings.push(new Reserva(cliente, viaje, dato.fecha))    
   })
 
   // Filtramos los datos si se proporcionan filtros
@@ -84,7 +84,6 @@ const getDB = (type, filters = {}) => {
     return db
   }
 }
-
 
 
 // Función para establecer o actualizar la base de datos en el localStorage
@@ -113,13 +112,12 @@ const setDB = obj => {
 
     // Si el tipo de objeto no es reconocido, mostramos un mensaje de error en la consola
     default: 
-      console.error("Tipo de objeto no reconocido") 
+      alert("Tipo de objeto no reconocido") 
   }
   
   // Guardamos la base de datos actualizada en el localStorage, convirtiéndola a formato JSON
   localStorage.setItem('db_AgenciaViajes', JSON.stringify(db))
 }
-
 
 
 // Función para manejar el envío del formulario
@@ -143,29 +141,33 @@ const post = event => {
   // Si se ha ingresado un código, se está creando un nuevo viaje
   } else if(data.codigo){
 
-    // Determinamos el tipo de viaje seleccionado
-    switch (data.selectTypeTrip) {
-      case "vuelo":
-        // Creamos un nuevo objeto Vuelo y lo guardamos en la base de datos
-        setDB(new Vuelo(data.codigo, data.destino, data.precio, "vuela rapido", Math.floor(Math.random() * 7) + 1))
-        break
+    if (data.precio > 0) {
+      // Determinamos el tipo de viaje seleccionado
+      switch (data.selectTypeTrip) {
+        case "vuelo":
+          // Creamos un nuevo objeto Vuelo y lo guardamos en la base de datos
+          setDB(new Vuelo(data.codigo, data.destino, data.precio, "vuela rapido", Math.floor(Math.random() * 7) + 1))
+          break
 
-      case "hotel":
-        // Creamos un nuevo objeto Hotel y lo guardamos en la base de datos
-        setDB(new Hotel(data.codigo, data.destino, data.precio, Math.floor(Math.random() * 5) + 0, "Individual" ))
-        break
+        case "hotel":
+          // Creamos un nuevo objeto Hotel y lo guardamos en la base de datos
+          setDB(new Hotel(data.codigo, data.destino, data.precio, Math.floor(Math.random() * 5) + 0, "Individual" ))
+          break
 
-      case "paquete":
-        // Creamos un nuevo objeto Paquete que incluye un vuelo y un hotel, y lo guardamos en la base de datos
-        const vuelo = new Vuelo(data.codigo, data.destino, data.precio, "vuela rapido", Math.floor(Math.random() * 7) + 1)
-        const hotel = new Hotel(data.codigo, data.destino, data.precio, Math.floor(Math.random() * 5) + 0, "Individual")
-        setDB(new Paquete(data.codigo, data.destino, data.precio, vuelo, hotel ))
-        break
-    
-      // Si el tipo de viaje no es reconocido, mostramos un mensaje de error en la consola
-      default:
-        console.error("No se ha encontrado ese tipo de viaje")
-        break
+        case "paquete":
+          // Creamos un nuevo objeto Paquete que incluye un vuelo y un hotel, y lo guardamos en la base de datos
+          const vuelo = new Vuelo(data.codigo, data.destino, data.precio, "vuela rapido", Math.floor(Math.random() * 7) + 1)
+          const hotel = new Hotel(data.codigo, data.destino, data.precio, Math.floor(Math.random() * 5) + 0, "Individual")
+          setDB(new Paquete(data.codigo, data.destino, data.precio, vuelo, hotel ))
+          break
+      
+        // Si el tipo de viaje no es reconocido, mostramos un mensaje de error en la consola
+        default:
+          alert("No se ha encontrado ese tipo de viaje")
+          break
+      }
+    }else{
+      alert("El precio tiene que ser un valor superior a 0")
     }
 
   // Si se ha seleccionado un cliente y un tipo de viaje, se está creando una nueva reserva
@@ -184,7 +186,7 @@ const post = event => {
 
     } else {
       // Si no se encontró el cliente o el viaje, mostramos un mensaje de error en la consola
-      console.error("No se encontró el cliente o el viaje.")
+      alert("No se encontró el cliente o el viaje.")
     }
     
   }
@@ -196,55 +198,82 @@ const post = event => {
 
 // Función para eliminar una fila de la tabla
 const deleteRow = boton => {
-  // Obtenemos la fila de la tabla donde se hizo clic en el botón
-  const rowTable = boton.parentElement.parentElement
-  // Obtenemos el ID de la tabla a la que pertenece la fila
-  const tableId = rowTable.parentElement.id 
 
-  // Recuperamos la base de datos actual desde el localStorage
-  const db = getDB() 
+  if (window.confirm("Estas seguro que quieres eliminar este elemento?")) {
+    // Obtenemos la fila de la tabla donde se hizo clic en el botón
+    const rowTable = boton.parentElement.parentElement
+    // Obtenemos el ID de la tabla a la que pertenece la fila
+    const tableId = rowTable.parentElement.id 
 
-  let elementoId
+    // Recuperamos la base de datos actual desde el localStorage
+    const db = getDB() 
 
-  // Determinamos qué tabla se está utilizando para eliminar el elemento
-  switch (tableId) {
-    case 'tableClients':
-      // Obtenemos el email del cliente desde la celda correspondiente
-      elementoId = rowTable.cells[2].textContent 
-      // Filtramos la lista de clientes para eliminar el que coincide con el email
-      db.clients = db.clients.filter(client => client.email !== elementoId)
-      break
+    // Determinamos qué tabla se está utilizando para eliminar el elemento
+    switch (tableId) {
+      case 'tableClients':
+        // Obtenemos el email del cliente desde la celda correspondiente
+        const email = rowTable.cells[2].textContent 
 
-    case 'tableTrips':
-      // Obtenemos el código del viaje desde la celda correspondiente
-      elementoId = rowTable.cells[0].textContent 
-      // Filtramos la lista de viajes para eliminar el que coincide con el código
-      db.trips = db.trips.filter(trip => trip.codigo !== elementoId)
-      break
+        //Comprobamos en la db si el cliente tiene una reserva realizada
+        const bookingsFind = db.bookings.some(booking => { 
+          const nameClient = `${booking.cliente.nombre} ${booking.cliente.apellido}`
+          const client = db.clients.find(c => c.email === email)
+          const completName = `${client.nombre} ${client.apellido}`
+          return nameClient === completName
+        })
 
-    case 'tableBooking':
-      // Obtenemos el nombre completo del cliente y el destino desde las celdas correspondientes
-      const nombreCompleto = rowTable.cells[0].textContent 
-      const destino = rowTable.cells[1].textContent 
+        //Si el usaurio tiene reservas a su nombre se le explica la cancelacion del proceso con un alert
+        if (bookingsFind) { 
+          alert("No se puede eliminar el cliente porque tiene reservas asociadas")
+          return
+        }
 
-      // Filtramos la lista de reservas para eliminar la que coincide con el cliente y el destino
-      db.bookings = db.bookings.filter(booking => {
-        const nombreCliente = `${booking.cliente.nombre} ${booking.cliente.apellido}`
-        return nombreCliente !== nombreCompleto || booking.viaje.destino !== destino
-      })
-      break
+        // Filtramos la lista de clientes para eliminar el que coincide con el email
+        db.clients = db.clients.filter(client => client.email !== email)
+        break
 
-    default:
-      // Si la tabla no es reconocida, mostramos un mensaje de error en la consola
-      console.error('Tabla desconocida')
-      return
+      case 'tableTrips':
+        // Obtenemos el código del viaje desde la celda correspondiente
+        const code = rowTable.cells[0].textContent 
+        
+        //Comprobamos en la db si el cliente tiene una reserva realizada
+        const bookingsFindTrip = db.bookings.some(booking => booking.viaje.codigo === code )
+        
+        //Si el usaurio tiene reservas a su nombre se le explica la cancelacion del proceso con un alert
+        if (bookingsFindTrip) { 
+          alert("No se puede eliminar el viaje porque tiene reservas asociadas")
+          return
+        }
+
+        // Filtramos la lista de viajes para eliminar el que coincide con el código
+        db.trips = db.trips.filter(trip => trip.codigo !== code)
+        break
+
+      case 'tableBooking':
+        // Obtenemos el nombre completo del cliente y el destino desde las celdas correspondientes
+        const nombreCompleto = rowTable.cells[0].textContent 
+        const destino = rowTable.cells[1].textContent 
+
+        // Filtramos la lista de reservas para eliminar la que coincide con el cliente y el destino
+        db.bookings = db.bookings.filter(booking => {
+          const nombreCliente = `${booking.cliente.nombre} ${booking.cliente.apellido}`
+          return nombreCliente !== nombreCompleto || booking.viaje.destino !== destino
+        })
+        break
+
+      default:
+        // Si la tabla no es reconocida, mostramos un mensaje de error en la consola
+        console.error('Tabla desconocida')
+        return
+    }
+
+    // Guardamos la base de datos actualizada en el localStorage
+    localStorage.setItem('db_AgenciaViajes', JSON.stringify(db))
+
+    // Actualizamos la vista para reflejar los cambios en la base de datos
+    viewData()
   }
-
-  // Guardamos la base de datos actualizada en el localStorage
-  localStorage.setItem('db_AgenciaViajes', JSON.stringify(db))
-
-  // Actualizamos la vista para reflejar los cambios en la base de datos
-  viewData()
+  
 }
 
 
@@ -273,63 +302,89 @@ const viewData = () => {
   // Añadimos la opción por defecto en las listas desplegables
   selectClient.appendChild(new Option('Seleccionar Cliente', null, true, true))
   selectTrip.appendChild(new Option('Seleccionar Viaje', null, true, true))
+ 
+  //Comprueba si hay algun clientes en la bd
+  if (db.clients.length) {
 
-  // Llenamos la tabla de clientes con los datos de la base de datos
-  db.clients.forEach(client => {
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = ` 
-      <td>${client.nombre}</td> 
-      <td>${client.apellido}</td> 
-      <td>${client.email}</td> 
-      <td>${client.telefono}</td> 
-      <td> 
-        <button class="btnDelete">Eliminar</button> 
-      </td> 
-    `
-    tableClients.appendChild(newRow)
+    // Llenamos la tabla de clientes con los datos de la base de datos
+    db.clients.forEach(client => {
+      const newRow = document.createElement('tr')
+      newRow.innerHTML = ` 
+        <td>${client.nombre}</td> 
+        <td>${client.apellido}</td> 
+        <td>${client.email}</td> 
+        <td>${client.telefono}</td> 
+        <td> 
+          <button class="btnDelete">Eliminar</button> 
+        </td> 
+      `
+      tableClients.appendChild(newRow)
 
-    // Añadimos el cliente a la lista desplegable
-    const newOption = document.createElement('option')
-    newOption.value = client.email
-    newOption.textContent = `${client.nombre} ${client.apellido}`
-    selectClient.appendChild(newOption)
-  })
+      // Añadimos el cliente a la lista desplegable
+      const newOption = document.createElement('option')
+      newOption.value = client.email
+      newOption.textContent = `${client.nombre} ${client.apellido}`
+      selectClient.appendChild(newOption)
+    })
 
-  // Llenamos la tabla de viajes con los datos de la base de datos
-  db.trips.forEach(trip => {
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = ` 
-      <td>${trip.codigo}</td> 
-      <td>${trip.destino}</td> 
-      <td>${trip.precio}</td> 
-      <td>${trip.constructor.name}</td> 
-      <td> 
-        <button class="btnDelete">Eliminar</button> 
-      </td> 
-    `
-    tableTrips.appendChild(newRow)
+  }else(
+    //Si no hay ningun cliente se lo comunica al usuario en la tabla de Clientes
+    tableClients.innerHTML = "<h3>No se ha encontrado ningun Cliente</h3>"
+  )
 
-    // Añadimos el viaje a la lista desplegable
-    const newOption = document.createElement('option')
-    newOption.value = trip.codigo
-    newOption.textContent = trip.codigo
-    selectTrip.appendChild(newOption)
-  })
-
-  // Llenamos la tabla de reservas con los datos de la base de datos
-  db.bookings.forEach(booking => {
-    const newRow = document.createElement('tr')
+  //Comprueba si hay algun viajes en la bd
+  if (db.trips.length) {
     
-    newRow.innerHTML = ` 
-      <td>${booking.cliente.nombre} ${booking.cliente.apellido}</td> 
-      <td>${booking.viaje.destino}</td> 
-      <td>${booking.fecha}</td> 
-      <td> 
-        <button class="btnDelete">Eliminar</button> 
-      </td> 
-    `
-    tableBooking.appendChild(newRow)
-  })
+    // Llenamos la tabla de viajes con los datos de la base de datos
+    db.trips.forEach(trip => {
+      const newRow = document.createElement('tr')
+      newRow.innerHTML = ` 
+        <td>${trip.codigo}</td> 
+        <td>${trip.destino}</td> 
+        <td>${trip.precio}</td> 
+        <td>${trip.constructor.name}</td> 
+        <td> 
+          <button class="btnDelete">Eliminar</button> 
+        </td> 
+      `
+      tableTrips.appendChild(newRow)
+
+      // Añadimos el viaje a la lista desplegable
+      const newOption = document.createElement('option')
+      newOption.value = trip.codigo
+      newOption.textContent = trip.codigo
+      selectTrip.appendChild(newOption)
+    })
+    
+  } else {
+    //Si no hay ningun viaje se lo comunica al usuario en la tabla de Viaje
+    tableTrips.innerHTML = "<h3>No se ha encontrado ningun Viaje</h3>"
+  }
+
+  //Comprueba si hay alguna reserva en la bd
+  
+  if (db.bookings.length) {
+    
+    // Llenamos la tabla de reservas con los datos de la base de datos
+    db.bookings.forEach(booking => {
+      const newRow = document.createElement('tr')
+      
+      newRow.innerHTML = ` 
+        <td>${booking.cliente.nombre} ${booking.cliente.apellido}</td> 
+        <td>${booking.viaje.destino}</td> 
+        <td>${booking.fecha}</td> 
+        <td> 
+          <button class="btnDelete">Eliminar</button> 
+        </td> 
+      `
+      tableBooking.appendChild(newRow)
+    })
+    
+  } else {
+    //Si no hay ninguna reserva se lo comunica al usuario en la tabla de reservas
+    tableBooking.innerHTML = "<h3>No se ha encontrado ninguna Reserva</h3>"
+  }
+
 
   // Añadimos un evento de clic a cada botón de eliminar
   document.querySelectorAll('.btnDelete').forEach(button => {
