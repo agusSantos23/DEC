75286374bf895d0b141a66afaaf3addec8caf434
agusSantos23 <131393036+agusSantos23@users.noreply.github.com>
2025-01-10 17:47:15 +0100

    configuration datatable

--- js/main.js
+++ js/main.js
@@ -150,5 +150,26 @@ $("#myTable").DataTable({
     { "data": "id" }, 
     { "data": "email" }, 
     { "data": "registerDate" }
-  ]
+  ],
+  language: {
+    processing: "Procesando...",
+    search: "Buscar:",
+    lengthMenu: "Mostrar _MENU_ registros",
+    info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+    infoFiltered: "(filtrado de un total de _MAX_ registros)",
+    loadingRecords: "Cargando...",
+    zeroRecords: "No se encontraron resultados",
+    emptyTable: "Ningún dato disponible en esta tabla",
+    paginate: {
+      first: "Primero",
+      previous: "Anterior",
+      next: "Siguiente",
+      last: "Último"
+    },
+    aria: {
+      sortAscending: ": Activar para ordenar la columna de manera ascendente",
+      sortDescending: ": Activar para ordenar la columna de manera descendente"
+    }
+  }
 })
